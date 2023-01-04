bb5e9714d74819070ff21888aa6bcb18f5eea2dc
kientran2001 <kien.tv194311@gmail.com>
2023-01-04 20:50:47 +0700

    count deleted

--- src/app/controllers/MeController.js
+++ src/app/controllers/MeController.js
@@ -4,11 +4,26 @@ class MeController {
 
     // GET me/stored/courses
     storedCourses(req, res, next) {
-        Course.find({})
-            .then(courses => res.render('me/stored-courses', {
-                courses: multipleMongooseToObject(courses)
-            }))
-            .catch(err => next(err))
+        Promise.all([Course.find({}), Course.countDocumentsDeleted()])
+            .then(([courses, deletedCount]) => {
+                res.render('me/stored-courses', {
+                    deletedCount,
+                    courses: multipleMongooseToObject(courses)
+                })
+            })
+            .catch(next)
+
+        // Course.countDocumentsDeleted()
+        //     .then(deletedCount => {
+        //         console.log(deletedCount)
+        //     })
+        //     .catch(() => {})
+
+        // Course.find({})
+        //     .then(courses => res.render('me/stored-courses', {
+        //         courses: multipleMongooseToObject(courses)
+        //     }))
+        //     .catch(err => next(err))
     }
 
     // GET me/trash/courses
